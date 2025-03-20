eb83caabf3bf2b2ca999c5f21ffb444d175640e8
nubelonia <nubelonia@gmail.com>
2025-03-20 10:18:36 +0100

    Creación clase Vehiculo ej.1, ud.1

--- POOO/UD1/ej1/clases_ej1.py
+++ POOO/UD1/ej1/clases_ej1.py
@@ -4,3 +4,13 @@ Crear dos objetos pertenecientes a esa clase e imprimir en pantalla la marca, el
 modelo y el precio de cada vehículo (mediante __str__).
 
 '''
+
+class Vehiculo:
+    def __init__(self, marca, modelo, año, precio):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+        self.precio = precio
+        
+
+
