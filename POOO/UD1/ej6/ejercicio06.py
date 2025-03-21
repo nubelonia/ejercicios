88ef250e001cb0a5bee8ff852a0452ebf3f0f664
nubelonia <nubelonia@gmail.com>
2025-03-21 17:10:05 +0100

    ej6, ud1

--- POOO/UD1/ej6/ejercicio06.py
+++ POOO/UD1/ej6/ejercicio06.py
@@ -11,7 +11,12 @@ Producto("cebolla", "verdura", 1.8, 150),
 Producto("manzana", "fruta", 3.2, 50),
 Producto("pera", "fruta", 2.7, 75)]
 
+'''
+usamos for para iterar la lista de "productos"
+para ello se crea una variable temporal "producto", 
+que representa cada uno de los objetos (Producto) de la lista.
 
+'''
 for producto in productos:
     if producto.precio >=1.5 and producto.precio <= 2.5:
         print(producto)
