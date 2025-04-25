4eb38f47fa1760543db6ff30f4e0790ece75820f
nubelonia <nubelonia@gmail.com>
2025-04-25 22:39:38 +0200

    25-04-25 ofi

--- POOO/UD2/Benitez_Lopez_Manuel_POO_Tarea2/ejercicio2/clases.py
+++ POOO/UD2/Benitez_Lopez_Manuel_POO_Tarea2/ejercicio2/clases.py
@@ -1,1 +1,67 @@
-#Clases ejercicio 2 ...
+class Cadena:
+    def __init__(self, texto):
+        self.texto = texto
+
+    # a. Sumar objetos Cadena.
+    
+    def __add__(self, otro):
+        return self.texto + otro.texto
+    
+
+    '''
+    b. Restar objetos Cadena. Al restar dos objetos Cadena, las vocales de la
+segunda cadena se reemplazarán en la primera cadena por cadena vacía.
+Por tanto, el primer objeto Cadena ve modificado el valor de su atributo.
+Estudia el ejemplo:
+    
+    '''
+
+    
+    def __sub__(self, otro):
+        vocales_a_eliminar = ""
+        for letra in otro.texto:
+            if letra.lower() in "aeiou" and letra not in vocales_a_eliminar:
+                vocales_a_eliminar += letra
+
+        nuevo_texto = ""
+        for letra in self.texto:
+            if letra not in vocales_a_eliminar:
+                nuevo_texto += letra
+
+        self.texto = nuevo_texto
+        return self.texto
+
+
+
+
+
+
+    '''
+    c. Longitud de un objeto Cadena. La longitud de una instancia de Cadena es
+el número de vocales que tiene, incluidas mayúsculas y minúsculas con o
+sin tilde.
+    
+    '''
+
+
+
+
+
+    '''
+    d. Compara objetos Cadena. Dos objetos Cadena son iguales si tienen la
+misma longitud, es decir, el mismo número de vocales.
+    
+    '''
+
+
+
+
+    '''
+    e. Operador +=. Al realizar la operación cadena += valor_str, se añadirá al
+atributo del objeto Cadena la cadena valor_str. El objeto original se verá
+modificado.
+    
+    '''
+
+
+
--- POOO/UD2/Benitez_Lopez_Manuel_POO_Tarea2/ejercicio2/pruebas.py
+++ POOO/UD2/Benitez_Lopez_Manuel_POO_Tarea2/ejercicio2/pruebas.py
@@ -1,1 +1,57 @@
-#Pruebas ejercicio 2 ...
+from clases import *
+
+# Sumar objetos Cadena
+cadena1 = Cadena("Me gusta programar en ")
+cadena2 = Cadena("Python")
+print(cadena1 + cadena2)
+
+# Salida
+# Me gusta programar en Python
+
+
+
+
+# Restar objetos Cadena
+cadena1 = Cadena("El temor de un hombre sabio")
+cadena2 = Cadena("tormenta")
+print(cadena1 - cadena2)
+print(cadena1)
+
+# Salida
+# El tmr d un hmbr sbi
+# El tmr d un hmbr sbi
+
+
+
+
+
+# Longitud de una instancia de Cadena
+cadena1 = Cadena("Leí 'La Rueda del Tiempo'")
+print(len(cadena1))
+
+# Salida
+# 10
+
+
+
+
+
+# Comparar objetos Cadena
+cadena1 = Cadena("Python")
+cadena2 = Cadena("Cobol")
+cadena3 = Cadena("Java")
+print(cadena1 == cadena2)
+print(cadena2 == cadena3)
+
+# Salida
+# False
+# True
+
+
+# Añadir valor str
+cadena = Cadena("Sayonara ")
+cadena += "baby"
+print(cadena)
+
+# Salida
+# Sayonara baby
