61a429cb75a526f68dca02cc2b9bb1393ab5a327
nubelonia <nubelonia@gmail.com>
2025-04-30 18:33:40 +0200

    30-04-25 ofi

--- POOO/UD2/Benitez_Lopez_Manuel_POO_Tarea2/ejercicio2/clases.py
+++ POOO/UD2/Benitez_Lopez_Manuel_POO_Tarea2/ejercicio2/clases.py
@@ -1,3 +1,10 @@
+'''
+a. Sumar objetos Cadena. Se obtendrá un objeto str cuyo contenido será el
+resultado de concatenar las cadenas de los objetos Cadena con los que se
+opera.
+
+'''
+
 class Cadena:
     def __init__(self, texto):
         self.texto = texto
@@ -19,15 +26,20 @@ Estudia el ejemplo:
     
     def __sub__(self, otro):
         vocales_a_eliminar = ""
+        # El for con la variable "letra" recorre cada letra de la cadena2 = Cadena("tormenta")
         for letra in otro.texto:
+            #Si es vocal y aún no está añadida, se agrega a la variable vacía vocales_a_eliminar
             if letra.lower() in "aeiou" and letra not in vocales_a_eliminar:
                 vocales_a_eliminar += letra
 
+
         nuevo_texto = ""
+        # El for recorre cada letra de la cadena1 = Cadena("El temor de un hombre sabio")
         for letra in self.texto:
+            # Cada letra que no este en vocales_a_eliminar la añade a nuevo_texto
             if letra not in vocales_a_eliminar:
                 nuevo_texto += letra
-
+        # Reemplaza el texto original de la cadena1 por el nuevo_texto creado previamente
         self.texto = nuevo_texto
         return self.texto
 
